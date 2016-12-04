464ed6cbc5bbefe3cda80d2df86928751e9918b6
Hozaifa Abdalla <Abdallahozaifa19527@gmail.com>
2016-12-04 18:10:42 -0500

    added login page info retrieval

--- js/attendia.js
+++ js/attendia.js
@@ -2,8 +2,14 @@ $(document).ready(function(){
 	console.log("Attendia JS Loaded!!");
 
 	var signInBtn = $(".signin-btn");
-	var signUpBtn = $(".signup-btn");
-
+	var signUpBtn = $(".signup-btn"); 
+	var leftArrowBtn = $(".pull-left");
+	var fullName = $(".full-name");
+	var email = $(".email");
+	var userName = $(".username");
+	var password = $(".password");
+        var submitBtn = $(".submitBtn");
+        
 	signInBtn.click(function(){
 		window.location.href = "signin.html";
 	});
@@ -11,4 +17,26 @@ $(document).ready(function(){
 	signUpBtn.click(function(){
 		window.location.href = "signup.html";
 	});
+	
+	if(leftArrowBtn != null){
+	   leftArrowBtn.click(function(){
+	       window.location.href = "index.html";
+	   });
+	}
+	
+	if(submitBtn != null){
+	  submitBtn.click(function(){
+	     if(fullName.val() != null){
+	       console.log(fullName.val());
+	       console.log(email.val());
+	     }
+	     console.log(userName.val());
+	     console.log(password.val()); 
+	  }); 
+	}
+	
+	
+	
+	
+	
 });
